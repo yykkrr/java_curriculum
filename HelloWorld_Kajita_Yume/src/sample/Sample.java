ca8a8683d8fff90f9ecef66b83bc0a8e3eee07e4
kukurr <kajita.cor.r@gmail.com>
2024-03-29 21:32:41 +0900

    Java_curriculum_introduction

--- HelloWorld_Kajita_Yume/src/sample/Sample.java
+++ HelloWorld_Kajita_Yume/src/sample/Sample.java
@@ -1,10 +1,15 @@
 package sample;
 
-public class Sample {
+public class SelfIntroduction {
 
 	public static void main(String[] args) {
-			System.out.println("Hello World");
-			System.out.println("Hello World");
+		String name = "山田 花子";
+		int age = 25;
+		String hobby = "映画鑑賞";
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
 
 	}
 
