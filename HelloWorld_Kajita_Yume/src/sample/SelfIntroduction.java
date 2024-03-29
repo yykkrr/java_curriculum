e233a547dd1864c6cb1f3a5c5acb8f972c4aae65
kukurr <kajita.cor.r@gmail.com>
2024-03-29 21:47:12 +0900

    Java_SelfIntroduction

---
+++ HelloWorld_Kajita_Yume/src/sample/SelfIntroduction.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		String name = "山田 花子";
+		int age = 25;
+		String hobby = "映画鑑賞";
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}
